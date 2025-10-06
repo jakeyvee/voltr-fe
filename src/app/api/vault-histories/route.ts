566f6e671bbe0e8c968a239aa8522bc01a9446e9
jakeyvee <jakeyveee@gmail.com>
2025-10-06 09:42:19 +0800

    include tx type when indexing

--- src/app/api/vault-histories/route.ts
+++ src/app/api/vault-histories/route.ts
@@ -12,15 +12,53 @@ import { NextResponse } from "next/server";
 import { BN } from "@coral-xyz/anchor";
 import { getWritableAccountKeys } from "@/lib/addressLookupUtils";
 import { PROTOCOL_STATE_PUBKEY } from "@/lib/Constants";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { CompiledInstruction } from "@solana/web3.js";
+
+const INSTRUCTION_DISCRIMINATORS = new Map<string, string>([
+  ["161,145,203,248,211,202,203,67", "add_adaptor"],
+  ["178,116,38,9,23,20,91,154", "calibrate_high_water_mark"],
+  ["231,54,14,6,223,124,127,238", "cancel_request_withdraw_vault"],
+  ["56,247,170,246,89,221,134,200", "close_strategy"],
+  ["148,193,160,116,87,25,123,103", "create_lp_metadata"],
+  ["246,82,57,226,131,222,253,249", "deposit_strategy"],
+  ["126,224,21,255,228,53,117,33", "deposit_vault"],
+  ["119,33,54,52,194,8,211,239", "direct_withdraw_strategy"],
+  ["32,59,42,128,246,73,255,47", "harvest_fee"],
+  ["149,56,57,46,105,182,61,208", "init_or_update_protocol"],
+  ["248,207,228,15,13,191,43,58", "initialize_direct_withdraw_strategy"],
+  ["208,119,144,145,178,57,105,252", "initialize_strategy"],
+  ["48,191,163,44,71,129,63,164", "initialize_vault"],
+  ["161,199,99,22,25,193,61,193", "remove_adaptor"],
+  ["248,225,47,22,116,144,23,143", "request_withdraw_vault"],
+  ["67,229,185,188,226,11,210,60", "update_vault"],
+  ["31,45,162,5,193,217,134,188", "withdraw_strategy"],
+  ["135,7,237,120,149,94,95,7", "withdraw_vault"],
+]);
 
 export interface Transaction {
   transaction: {
     message: {
       accountKeys: string[];
       addressTableLookups: MessageAddressTableLookup[];
+      instructions: CompiledInstruction[];
     };
     signatures: string[];
   };
+}
+
+function findInstructionByDiscriminator(
+  instructionsDataBytes: number[][]
+): string[] {
+  const instructionArray = [];
+  for (const dataBytes of instructionsDataBytes) {
+    const key = dataBytes.join(",");
+    const instruction = INSTRUCTION_DISCRIMINATORS.get(key);
+    if (instruction) {
+      instructionArray.push(instruction);
+    }
+  }
+  return instructionArray;
 }
 
 async function fetchVaultsByAccountKeys(accountKeys: string[]) {
@@ -40,9 +78,20 @@ async function updateVaultHistory(
   connection: Connection,
   voltrClient: VoltrClient,
   vaultPk: PublicKey,
+  instructions: CompiledInstruction[],
   signature: string
 ) {
   const { vaultLpMint } = voltrClient.findVaultAddresses(vaultPk);
+
+  const instructionsDataHex = instructions.map((ix) => ix.data);
+
+  const instructionsDataBytes = instructionsDataHex.map((base58String) =>
+    Array.from(bs58.decode(base58String)).slice(0, 8)
+  );
+
+  const matchedInstructions = findInstructionByDiscriminator(
+    instructionsDataBytes
+  );
 
   // Fetch vault and mint data in parallel
   const [vaultAccount, vaultLpMintAccount] = await Promise.all([
@@ -77,6 +126,7 @@ async function updateVaultHistory(
       issuance_fee_bps: vaultAccount.feeConfiguration.issuanceFee,
       redemption_fee_bps: vaultAccount.feeConfiguration.redemptionFee,
       updated_tx: signature,
+      tx_type: matchedInstructions,
     });
 
   if (insertError) {
@@ -95,7 +145,10 @@ export async function POST(req: Request) {
     await Promise.all(
       transactions.map(async (tx) => {
         // Skip if transaction doesn't have required data
-        if (!tx.transaction?.message?.accountKeys) {
+        if (
+          !tx.transaction?.message?.accountKeys ||
+          !tx.transaction?.message?.instructions
+        ) {
           return;
         }
 
@@ -135,6 +188,7 @@ export async function POST(req: Request) {
               connection,
               vc,
               new PublicKey(vault.pk),
+              tx.transaction.message.instructions,
               tx.transaction.signatures[0]
             )
           )
