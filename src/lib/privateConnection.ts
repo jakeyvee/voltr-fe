5069d1a83aeb462fcba8c4558fbcfc7a23a13a7f
jakeyvee <jakeyveee@gmail.com>
2025-11-29 00:18:26 +0800

    remove api

--- src/lib/privateConnection.ts
+++
@@ -1,5 +0,0 @@
-import { Connection } from "@solana/web3.js";
-
-export function createConnection(): Connection {
-  return new Connection(process.env.SOLANA_RPC_URL!);
-}
--- src/lib/validate.ts
+++
@@ -1,7 +0,0 @@
-export async function validateAuthHeader(
-  authHeader: string | null
-): Promise<void> {
-  if (authHeader !== process.env.HELIUS_WEBHOOK_AUTH_HEADER) {
-    throw new Error("Invalid authentication header");
-  }
-}
